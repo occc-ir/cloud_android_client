cd2bb3d634ab154799471e6f7bf4ea9eb04fde51
Babaei <ahmad.babaei@gmail.com>
2014-10-28 18:04:10 +0330

    Fix bugs. Add RTL support.

--- src/ir/occc/android/wiki/WikiFragment.java
+++ src/ir/occc/android/wiki/WikiFragment.java
@@ -7,7 +7,6 @@ import ir.occc.android.adapter.WikiPageAdapter;
 import ir.occc.android.common.Common;
 import ir.occc.android.common.QueryType;
 import ir.occc.android.model.WikiPageItem;
-import ir.occc.android.rss.RssService;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,7 +15,6 @@ import java.util.List;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -44,15 +42,15 @@ public class WikiFragment extends BaseV4Fragment implements OnItemClickListener 
 	public String wikiTitle;
 
 	public WikiFragment(){}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		/** Getting the arguments to the Bundle object *//*
 		Bundle data = getArguments();
 
-		*//** Getting data of the key current_page from the bundle *//*
+		 *//** Getting data of the key current_page from the bundle *//*
 		try {
 			if (data.getString("content") != null && data.getString("content").length() > 0) {
 				viewWikiContent(data.getString("content"));
@@ -71,34 +69,46 @@ public class WikiFragment extends BaseV4Fragment implements OnItemClickListener 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		if (rootView == null) {
+			rootView = inflater.inflate(R.layout.fragment_wiki, container, false);
+			progressBar = (ProgressBar) rootView.findViewById(R.id.progressBar1);
+			webView = (WebView)rootView.findViewById(R.id.webViewWiki);
+			mainLayout = ((LinearLayout)webView.getParent());
 
-		rootView = inflater.inflate(R.layout.fragment_wiki, container, false);
-		progressBar = (ProgressBar) rootView.findViewById(R.id.progressBar1);
-		webView = (WebView)rootView.findViewById(R.id.webViewWiki);
-		mainLayout = ((LinearLayout)webView.getParent());
-		
-		search(queryType, wikiTitle);
+			search(queryType, wikiTitle);
+		}
+		else {
+			// If we are returning from a configuration change:
+			// "view" is still attached to the previous view hierarchy
+			// so we need to remove it and re-attach it to the current one
+			ViewGroup parent = (ViewGroup) rootView.getParent();
+			parent.removeView(rootView);
+		}
 
 		return rootView;
 	}
-	
+
 	public void refresh(QueryType type, String query) {
 		stopService(WikiService.class);
-		search(queryType, query);
+		search(type, query);
 	}
-	
+
 	@Override
 	protected void search(QueryType type, String query) {
 		this.queryType = type;
 		this.queryText = query;
-		
+
 		switch (type) {
 		case WikiTitle:
-			startService(query);
+			startService(queryText, WikiService.CMD_WIKI_SEARCH_PAGE);
 			break;
-			
+
 		case WikiContent:
-			startService(query);
+			startService(queryText, WikiService.CMD_WIKI_SEARCH_PAGE);
+			break;
+
+		case WikiSpecialPage:
+			startService(queryText, WikiService.CMD_WIKI_EXACT_PAGE);
 			break;
 			
 		default:
@@ -106,7 +116,7 @@ public class WikiFragment extends BaseV4Fragment implements OnItemClickListener 
 		}
 	}
 
-	private void startService(String title) {
+	private void startService(String title, int cmd) {
 		mainLayout.setGravity(Gravity.CENTER);
 		progressBar.setVisibility(View.VISIBLE);
 		webView.setVisibility(View.GONE);
@@ -115,23 +125,31 @@ public class WikiFragment extends BaseV4Fragment implements OnItemClickListener 
 		}
 
 		Intent intent = new Intent(getActivity(), WikiService.class);
-		intent.putExtra(Common.COMMAND, WikiService.CMD_WIKI_SEARCH_PAGE);			
+		//intent.putExtra(Common.COMMAND, WikiService.CMD_WIKI_SEARCH_PAGE);
+		intent.putExtra(Common.COMMAND, cmd);
 		intent.putExtra(WikiService.WIKI_TITLE, title);
-		
+
 		startService(WikiService.class, intent);
 	}
 
 	protected void receivedResult(int resultCode, Bundle resultData) {
 		@SuppressWarnings("unchecked")
-		List<Page> listOfPages = (List<Page>) resultData.getSerializable(RssService.ITEMS);
+		List<Page> listOfPages = (List<Page>) resultData.getSerializable(WikiService.ITEMS);
 
-		if (listOfPages.size() == 0) {
+		if (listOfPages == null || listOfPages.size() == 0) {
+
+			if (isDetached() || isRemoving() || isHidden() || !isVisible()) {
+				return;
+			}
+
 			viewNoContent(getString(R.string.page_not_found));
-		/*} else if (listOfPages.size() == 1) {
+		} 
+		/*else if (listOfPages.size() == 1) {
 			for (Page page : listOfPages) {
 				viewWikiContent(page.getCurrentContent());
-			}	*/
-		} else {
+			}	
+		}*/ 
+		else {
 			List<WikiPageItem> items = new ArrayList<WikiPageItem>();
 			for (Page page : listOfPages) {
 				try {
@@ -139,7 +157,7 @@ public class WikiFragment extends BaseV4Fragment implements OnItemClickListener 
 					String pageContent = page.getCurrentContent();
 					int pageId = Integer.parseInt(page.getPageid());
 					//int len = pageContent.length() < 70 ? pageContent.length() : 70;
-					
+
 					WikiPageItem item = new WikiPageItem(pageTitle, pageContent, pageId);
 					items.add(item);
 				} catch (Exception e) {
@@ -148,21 +166,21 @@ public class WikiFragment extends BaseV4Fragment implements OnItemClickListener 
 			}
 
 			WikiPageAdapter adapter = new WikiPageAdapter(getActivity(), items);
-			
+
 			lvPages = null;
 			if (lvPages == null) {
-				
+
 				lvPages = new ListView(getActivity());
 				mainLayout.addView(lvPages);
 				mainLayout.setGravity(Gravity.TOP);
 			}
-			
+
 			lvPages.setAdapter(adapter);
 			lvPages.setOnItemClickListener(this);
 
 			webView.setVisibility(View.GONE);
 			progressBar.setVisibility(View.GONE);
-			
+
 			if (listOfPages.size() == 1) {
 				if (adapter.getCount() == 0) {
 					viewNoContent(getString(R.string.query_has_problem));
@@ -172,18 +190,27 @@ public class WikiFragment extends BaseV4Fragment implements OnItemClickListener 
 			}
 		}
 	}
-	
-	private void viewSpecialPage() {
-		
+
+	public void viewSpecialPage() {
+		Intent intent = new Intent(getActivity(), WikiService.class);
+		intent.putExtra(Common.COMMAND, WikiService.CMD_WIKI_EXACT_PAGE);			
+		intent.putExtra(WikiService.WIKI_TITLE, " ");
+
+		startService(WikiService.class, intent);
 	}
-	
+
 	private void viewNoContent(String text) {
 
 		StringBuilder sbMessage = new StringBuilder();
-		sbMessage.append("<html><body dir='rtl'><div style='text-align:center'>");
+		if (Common.isRtlRequired(text)) {
+			sbMessage.append("<html><body dir='rtl'><div style='text-align:center'>");
+		}
+		else {
+			sbMessage.append("<html><body dir='ltr'><div style='text-align:center'>");
+		}
 		sbMessage.append(text);
 		sbMessage.append("</div></body></html>");
-		
+
 		webView.loadDataWithBaseURL("about:blank", sbMessage.toString(), "text/html", "utf-8", null);
 		webView.setVisibility(View.VISIBLE);
 
@@ -193,7 +220,7 @@ public class WikiFragment extends BaseV4Fragment implements OnItemClickListener 
 		}
 	}
 
-	
+
 	/**
 	 * Show the content of wiki page with title
 	 * 
@@ -221,7 +248,7 @@ public class WikiFragment extends BaseV4Fragment implements OnItemClickListener 
 	 */
 	private void viewWikiContent(int position, WikiPageAdapter adapter) {
 		WikiPageItem item = (WikiPageItem) adapter.getItem(position);
-		
+
 		Bundle bundle = new Bundle();
 		bundle.putString("title", item.getTitle());
 		bundle.putString("pubDate", "");
