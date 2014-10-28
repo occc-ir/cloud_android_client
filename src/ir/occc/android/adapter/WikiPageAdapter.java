69e6dbb80b201b74427705ad5ac9ba80012c8d25
Babaei <ahmad.babaei@gmail.com>
2014-10-28 18:00:43 +0330

    UI improvement for RTL text.

--- src/ir/occc/android/adapter/WikiPageAdapter.java
+++ src/ir/occc/android/adapter/WikiPageAdapter.java
@@ -1,6 +1,7 @@
 package ir.occc.android.adapter;
 
 import ir.occc.android.R;
+import ir.occc.android.common.Common;
 import ir.occc.android.model.WikiPageItem;
 
 import java.util.List;
@@ -11,6 +12,8 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 
 public class WikiPageAdapter extends BaseAdapter {
@@ -52,6 +55,27 @@ public class WikiPageAdapter extends BaseAdapter {
 		}
 		holder.itemTitle.setText(items.get(position).getTitle());
 		holder.itemContent.setText(items.get(position).getContent());
+		
+		// set relative layout params relate on RTL or LTR
+		RelativeLayout.LayoutParams params = new LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+		if (Common.isRtlRequired(items.get(position).getTitle())) {
+			params.addRule(RelativeLayout.ALIGN_PARENT_TOP, R.id.listItemWikiRelativeLayout);
+			params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, R.id.listItemWikiRelativeLayout);
+		} else {
+			params.addRule(RelativeLayout.ALIGN_PARENT_TOP, R.id.listItemWikiRelativeLayout);
+			params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, R.id.listItemWikiRelativeLayout);
+		}
+		holder.itemTitle.setLayoutParams(params);
+		
+		params = new LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+		if (Common.isRtlRequired(items.get(position).getContent())) {
+			params.addRule(RelativeLayout.BELOW, R.id.postTitleLabel);
+			params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, R.id.postTitleLabel);
+		} else {
+			params.addRule(RelativeLayout.BELOW, R.id.postTitleLabel);
+			params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, R.id.postTitleLabel);
+		}
+		holder.itemContent.setLayoutParams(params);
 
 		Animation anim = AnimationUtils.loadAnimation(parent.getContext(), R.anim.push_up_in);
 		convertView.startAnimation(anim);
